d2fbcd5f98f881120afcade9e964cbd6e9dd1b15
Keshav <keshavp@utexas.edu>
2022-03-29 17:54:37 -0500

    Slight update as I rework code...

--- ASE382Q11_hw5_kpp448.py
+++ ASE382Q11_hw5_kpp448.py
@@ -11,15 +11,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Establishing initial characteristics
-h = np.array([0.01, 0.005, 0.001, 0.0005]) #distance between grid points
+h = np.array([0.1, 0.005, 0.001, 0.0005]) #distance between grid points
 k = np.array([0.01, 0.005, 0.001, 0.0005]) #distance between time steps
 c = 1 #value of c in determining courant number
-N = 1/k #arbitrary number of elements in x for now
+N = 1/k #arbitrary number of time steps for now
+
+#variables for looping through everything
+iterate = True
+hval = 0
+kval = 0
 
 #the following loop will iterate through time using values prescribed above
-iterate = True
-i = 1
-j = 1
-while(iterate is True):
-    x = np.linspace(0,1,1/h(i))
-#Part 1: Euler Explicit Upwind
+#while(iterate is True):
+x = np.linspace(0,1,int(1/h[hval] + 1)) #gives us domain of integration (i.e. number of rows)
+ex = np.zeros((np.size(x),2)) #create the vector with which we solve to the end
+#lw = np.zeros((np.size(x),2))
+#bw = np.zeros((np.size(x),2))
+sigma = c*k[kval]/h[hval] #prescribe a value of k for this iteration based off sigma
+
+for i in range(len(x)):
+    ex[i,0] = np.sin(4*np.pi*x[i])
+    #lw[i,0] = np.sin(4*np.pi*x[i])
+    #bw[i,0] = np.sin(4*np.pi*x[i])
+
